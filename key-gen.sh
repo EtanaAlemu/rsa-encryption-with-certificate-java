14d8750014de32c09dac6d89fb77f3d48800a33e
EtanaAlemu <etanaalemunew@gmail.com>
2023-12-29 23:15:11 -0800

    bash command to generate key

--- key-gen.sh
+++ key-gen.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
+
 # Store passwords in variables
-keystore_password="8e0d049a2b55426badd6e47005e52e2f"
-key_password="4bf5bdc900fbf6e6506be4b052bf2d99"
-truststore_password="b6f810c59f6a9458d0bff69050bbd1f7"
+private_key_file="cert1.private.key"
+public_certificate_file="cert1.public.crt"
+password="4bf5bdc900fbf6e6506be4b052bf2d99"
+keystore_file="card_management_keystore.p12"
+alias="mobile_card_management"
 
-# Generate key pair
-keytool -genkeypair -alias mobile_card_management -keyalg RSA -keysize 2048 -keystore card_management_keystore.jks -validity 365 -storepass "$keystore_password" -keypass "$key_password" -dname "CN=Mobile Card Management, OU=DxValley, O=Cooperative Bank of Oromia, L=Addis Ababa, ST=Addis Ababa, C=ET"
+# Generate private key and self-signed certificate
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$private_key_file" -out "$public_certificate_file" -subj "/CN=Mobile Card Management/OU=DxValley/O=Cooperative Bank of Oromia/L=Addis Ababa/ST=Addis Ababa/C=ET"
 
-# Export certificate
-keytool -export -alias mobile_card_management -keystore card_management_keystore.jks -file mobile_card_management.crt -storepass "$keystore_password"
-
-# Import certificate into truststore (without prompting)
-keytool -import -alias mobile_card_management -file mobile_card_management.crt -keystore card_management_truststore.jks -storepass "$truststore_password" -noprompt
+# Export certificate as PKCS12 with legacy provider
+openssl pkcs12 -export -inkey "$private_key_file" -in "$public_certificate_file" -out "$keystore_file" -name "$alias" -passout pass:"$password"
