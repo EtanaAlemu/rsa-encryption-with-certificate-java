f6efce8d852fe4b47937c9103d2528940462e7f2
EtanaAlemu <etanaalemunew@gmail.com>
2023-12-29 23:14:30 -0800

    modify for JDK 1.8 add Base64 encoding

--- key-gen.bat
+++ key-gen.bat
@@ -1,15 +1,14 @@
 @echo off
 
-rem Store passwords in variables
-set "keystore_password=8e0d049a2b55426badd6e47005e52e2f"
-set "key_password=4bf5bdc900fbf6e6506be4b052bf2d99"
-set "truststore_password=b6f810c59f6a9458d0bff69050bbd1f7"
+rem Store file paths in variables
+set "private_key_file=cert1.private.key"
+set "public_certificate_file=cert1.public.crt"
+set "password=4bf5bdc900fbf6e6506be4b052bf2d99"
+set "keystore_file=card_management_keystore.p12"
+set "alias=mobile_card_management"
 
-rem Generate key pair
-keytool -genkeypair -alias mobile_card_management -keyalg RSA -keysize 2048 -keystore card_management_keystore.jks -validity 365 -storepass %keystore_password% -keypass %key_password% -dname "CN=Mobile Card Management, OU=DxValley, O=Cooperative Bank of Oromia, L=Addis Ababa, ST=Addis Ababa, C=ET"
+rem Generate private key and self-signed certificate
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "%private_key_file%" -out "%public_certificate_file%"  -subj "/CN=Mobile Card Management/OU=DxValley/O=Cooperative Bank of Oromia/L=Addis Ababa/ST=Addis Ababa/C=ET"
 
-rem Export certificate
-keytool -export -alias mobile_card_management -keystore card_management_keystore.jks -file mobile_card_management.crt -storepass %keystore_password%
-
-rem Import certificate into truststore (without prompting)
-keytool -import -alias mobile_card_management -file mobile_card_management.crt -keystore card_management_truststore.jks -storepass %truststore_password% -noprompt
+rem Export certificate as PKCS12 with legacy provider
+openssl pkcs12 -inkey "%private_key_file%" -in "%public_certificate_file%" -export -out "%keystore_file%" -name "%alias%" -legacy -passout pass:%password%
--- src/main/java/com/dxvalley/RSA.java
+++ src/main/java/com/dxvalley/RSA.java
@@ -1,109 +1,131 @@
 package com.dxvalley;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.*;
-import java.security.cert.CertificateException;
-import java.util.Arrays;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.util.Base64;
+import java.util.Enumeration;
 
 public class RSA {
 
     static PublicKey publicKey;
     static PrivateKey privateKey;
-    private static final String KEYSTORE_PATH = "C:\\Users\\Appconpc\\IdeaProjects\\RSA\\card_management_keystore.jks";
-    private static final String TRUSTSTORE_PATH = "C:\\Users\\Appconpc\\IdeaProjects\\RSA\\card_management_truststore.jks";
-    private static final String KEYSTORE_PASSWORD = "8e0d049a2b55426badd6e47005e52e2f";
-    private static final String TRUSTSTORE_PASSWORD = "b6f810c59f6a9458d0bff69050bbd1f7";
+    private static final String KEYSTORE_PATH = "C:\\Users\\Appconpc\\IdeaProjects\\RSA\\card_management_keystore.p12";
+    private static final String KEYSTORE_PASSWORD = "4bf5bdc900fbf6e6506be4b052bf2d99";
+    private static final String ALIAS = "mobile_card_management";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         try {
-        // Message to be encrypted
-        String payload = "Hello, RSA!";
+            // Message to be encrypted
+            String payload = "{\"Pan\":\"D9946D7527EDD05E640\",\"NewPin\":\"89E598689DCF5437\",\"SkipFeeCheck\":true,\"KeyReference\":\"EMBOSSING_ZPK\",\"KeyType\":\"ZPK\",\"PinBlockFormat\":1,\"Reason\":\"TESTPINSET\",\"PinType\":1}";
 
-        // Encrypt the message
-        byte[] encryptedBytes = encrypt(payload);
+            // Print results
+            System.out.println("Original Message: " + payload);
+            // Encrypt the message
+            String encryptedMessage = encrypt(payload);
 
-        // Decrypt the message
-        String decryptedMessage = decrypt(encryptedBytes);
+            System.out.println("Encrypted Message: " + encryptedMessage);
+            // Decrypt the message
+            String decryptedMessage = decrypt(encryptedMessage);
+            System.out.println("Decrypted Message: " + decryptedMessage);
 
-        // Print results
-        System.out.println("Original Message: " + payload);
-        System.out.println("Encrypted Message: " + new String(encryptedBytes));
-        System.out.println("Decrypted Message: " + decryptedMessage);
+            byte[] signedPayload = signPayload(payload);
 
-        byte[] signedPayload = signPayload(payload);
-
-        boolean verified = verifyPayload(signedPayload,payload);
-        if (verified) {
-            // Signature is valid
-            System.out.println("valid");
-        } else {
-            // Signature is invalid
-            System.out.println("invalid");
-        }
+            boolean verified = verifyPayload(signedPayload, payload);
+            if (verified) {
+                // Signature is valid
+                System.out.println("valid");
+            } else {
+                // Signature is invalid
+                System.out.println("invalid");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static byte[] encrypt(String message) throws Exception {
-        loadKeystoreTrustStore();
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding", "BC");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        return cipher.doFinal(message.getBytes());
+    public static String encrypt(String message) {
+        try {
+            loadKeystoreTrustStore();
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(message.getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public static String decrypt(byte[] encryptedBytes) throws Exception {
-        loadKeystoreTrustStore();
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-        return new String(decryptedBytes);
+    public static String decrypt(String payload) {
+        try {
+            loadKeystoreTrustStore();
+            byte[] decodedMessage = Base64.getDecoder().decode(payload);
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] decryptedBytes = cipher.doFinal(decodedMessage);
+            return new String(decryptedBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     // Verify the signature
-    public static boolean verifyPayload(byte[] signedPayload, String payload) throws Exception{
+    public static boolean verifyPayload(byte[] signedPayload, String payload) {
+        try {
             loadKeystoreTrustStore();
             Signature verifier = Signature.getInstance("SHA256withRSA");
             verifier.initVerify(publicKey);
             verifier.update(payload.getBytes());
             return verifier.verify(signedPayload);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     // Sign the payload
-    public static byte[] signPayload(String payload)throws Exception {
-        loadKeystoreTrustStore();
-        Signature signature = Signature.getInstance("SHA256withRSA");
-        signature.initSign(privateKey);
-        signature.update(payload.getBytes());
-        return signature.sign();
+    public static byte[] signPayload(String payload) {
+        try {
+            loadKeystoreTrustStore();
+            Signature signature = Signature.getInstance("SHA256withRSA");
+            signature.initSign(privateKey);
+            signature.update(payload.getBytes());
+            return signature.sign();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    private static void loadKeystoreTrustStore() throws Exception {
-        // Load Bouncy Castle provider
-        Security.addProvider(new BouncyCastleProvider());
-
-        try (FileInputStream keystoreStream = new FileInputStream(KEYSTORE_PATH);
-             FileInputStream truststoreStream = new FileInputStream(TRUSTSTORE_PATH)) {
-
-            KeyStore keyStore = KeyStore.getInstance("JKS");
+    private static void loadKeystoreTrustStore() {
+        try (FileInputStream keystoreStream = new FileInputStream(KEYSTORE_PATH)) {
+            KeyStore keyStore = KeyStore.getInstance("PKCS12"); // Specify the provider
             keyStore.load(keystoreStream, KEYSTORE_PASSWORD.toCharArray());
 
-            KeyStore trustStore = KeyStore.getInstance("JKS");
-            trustStore.load(truststoreStream, TRUSTSTORE_PASSWORD.toCharArray());
+            // Print all aliases in the keystore for debugging purposes
+            Enumeration<String> aliases = keyStore.aliases();
+            while (aliases.hasMoreElements()) {
+                System.out.println("Alias: " + aliases.nextElement());
+            }
 
             // Retrieve private and public keys from keystore
-            privateKey = (PrivateKey) keyStore.getKey("mobile_card_management", KEYSTORE_PASSWORD.toCharArray());
-            publicKey = trustStore.getCertificate("mobile_card_management").getPublicKey();
-        } catch (FileNotFoundException e) {
-            throw new FileNotFoundException("Keystore or truststore file not found. Check file paths.");
+            privateKey = (PrivateKey) keyStore.getKey(ALIAS, KEYSTORE_PASSWORD.toCharArray());
+            System.out.println("PrivateKey Algorithm: " + privateKey.getAlgorithm());
+            System.out.println("PrivateKey Format: " + privateKey.getFormat());
+            System.out.println("PrivateKey Encoded: " + Base64.getEncoder().encodeToString(privateKey.getEncoded()));
+
+            publicKey = keyStore.getCertificate(ALIAS).getPublicKey();
+            System.out.println("PublicKey Algorithm: " + publicKey.getAlgorithm());
+            System.out.println("PublicKey Format: " + publicKey.getFormat());
+            System.out.println("PublicKey Encoded: " + Base64.getEncoder().encodeToString(publicKey.getEncoded()));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
